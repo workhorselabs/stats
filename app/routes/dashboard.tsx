bf0a9473ca09b26969034bdbdf4fc7ede83e26f2
wonjae <workhorse.lab@gmail.com>
2025-03-20 15:42:26 -0700

    create dashboard layout

---
+++ app/routes/dashboard.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "@remix-run/react";
+import { AppSidebar } from "~/components/app-sidebar";
+import { SiteHeader } from "~/components/site-header";
+import { SidebarInset, SidebarProvider } from "~/components/ui/sidebar";
+
+export default function Layout() {
+  return (
+    <SidebarProvider>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <Outlet />
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
