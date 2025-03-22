16c9d1280bd9a68c7df87184d8fe1bd55fa6feea
wonjae <workhorse.lab@gmail.com>
2025-03-22 15:31:58 -0700

    set up about page

---
+++ app/routes/about._index.tsx
@@ -0,0 +1,5 @@
+import { About } from "~/components/about";
+
+export default function Page() {
+  return <About />;
+}
---
+++ app/routes/about.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from "@remix-run/react";
+import { LandingLayout } from "~/components/landing-layout";
+export default function Layout() {
+  return (
+    <LandingLayout>
+      <Outlet />
+    </LandingLayout>
+  );
+}
