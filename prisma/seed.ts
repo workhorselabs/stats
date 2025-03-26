f524e0734f9bdd94fc31b74de3e558eb3a699f3c
wonjae <workhorse.lab@gmail.com>
2025-03-26 00:59:55 -0700

    add seed script for reference

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const hashedPassword = await bcrypt.hash("password123", 10);
+
+  await prisma.user.create({
+    data: {
+      email: "test@example.com",
+      password: hashedPassword,
+    },
+  });
+
+  console.log("Seeded user!");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
