840dce1b6c9eabfa44f7fd95622d8c70df936497
wonjae <workhorse.lab@gmail.com>
2025-03-26 01:15:32 -0700

    add quotes to user table

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,7 +11,7 @@ export const meta: MetaFunction = () => {
 };
 
 export const loader = async () => {
-  const users = await query("SELECT id, email, createdAt FROM User");
+  const users = await query('SELECT id, email, "createdAt" FROM "User"');
 
   return json({ users });
 };
