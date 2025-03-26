fe91271591d165d0ecc5b3955c3897446df0defa
wonjae <workhorse.lab@gmail.com>
2025-03-26 01:09:38 -0700

    remove s from User table name

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,7 +11,7 @@ export const meta: MetaFunction = () => {
 };
 
 export const loader = async () => {
-  const users = await query("SELECT id, email, createdAt FROM Users");
+  const users = await query("SELECT id, email, createdAt FROM User");
 
   return json({ users });
 };
