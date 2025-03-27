4d2cb49fafbabb9037be6ef78dbfbefb13d24f30
wonjae <workhorse.lab@gmail.com>
2025-03-26 18:53:35 -0700

    remove log

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,9 +1,24 @@
-import { Outlet } from "@remix-run/react";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
 import { AppSidebar } from "~/components/app-sidebar";
 import { SiteHeader } from "~/components/site-header";
 import { SidebarInset, SidebarProvider } from "~/components/ui/sidebar";
+import { getSession } from "~/utils/session.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const session = await getSession(request.headers.get("cookie"));
+  console.log(" loader ~ session ===========>", session);
+  const user = session.get("user");
+
+  if (!user) return redirect("/login");
+
+  return json({ user });
+}
 
 export default function Layout() {
+  const { user } = useLoaderData<typeof loader>();
+
   return (
     <SidebarProvider>
       <AppSidebar variant="inset" />
