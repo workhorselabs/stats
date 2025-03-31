c8c698d81accc861bbbfe29065e0953918d212e7
wonjae <workhorse.lab@gmail.com>
2025-03-30 21:59:15 -0400

    update seeder

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -15,7 +15,7 @@ VALUES (
   '\$2b\$10\$7yJZg/9YJZuB6KQbl3t8xut12IlA6hUQ5swXuQ/kTlxaJ0X2dV2OO',
   NOW()
 )
-ON CONFLICT (id) DO UPDATE SET
+ON CONFLICT (id) DO UPDATE SET;
 
 -- Insert a dummy blog post
 INSERT INTO "Post" (id, title, slug, content, published, "authorId", "createdAt", "updatedAt")
